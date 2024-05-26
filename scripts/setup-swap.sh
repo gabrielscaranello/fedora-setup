e7400a5f744e386123adc8dd86600dbb8dc54b54
Gabriel Scaranello <gabrieljscaranello@gmail.com>
2024-05-26 01:40:17 -0300

    refactor: change swap configuration
    Setup to use BTRFS and update zram config

--- scripts/setup-swap.sh
+++ scripts/setup-swap.sh
@@ -1,6 +1,7 @@
 #! /bin/bash
 
 SIZE=$(cat </proc/meminfo | awk '/MemTotal/ { printf("%.0f", ($2 / 1024 / 1024 / 2)) }')
+MB_SIZE=$(echo "$SIZE" | awk '{ printf "%.0f", ($1 * 1024) }')
 
 echo "Setting up swap..."
 
@@ -8,23 +9,28 @@ echo "Removing existing swap..."
 sudo swapoff --all
 sudo rm -rf /swapfile
 sudo rm -rf /etc/sysctl.d/00-custom.conf
-sudo sed -i '/^zram-size/d' /etc/systemd/zram-generator.conf
-sudo sed -i '/^\/swapfile/d' /etc/fstab
+sudo sed -i '/zram0/d; /^zram-size/d; /^compression-algorithm/d' /etc/systemd/zram-generator.conf
+sudo sed -i '/^\/swapfile/d' /etc/fstab >/dev/null
 
 echo "Configuring zram-generator..."
-echo 'zram-size=max(ram/2, 4096)' | sudo tee -a /etc/systemd/zram-generator.conf
+sudo tee /etc/systemd/zram-generator.conf >/dev/null <<EOL
+[zram0]
+zram-size = max(ram/2, $MB_SIZE)
+compression-algorithm = lz4
+EOL
 
 echo "Creating swapfile"
-sudo fallocate -l ${SIZE}G /swapfile
-sudo chmod 600 /swapfile
-sudo mkswap /swapfile
-echo '/swapfile none swap sw 0 0' | sudo tee -a /etc/fstab
+sudo btrfs filesystem mkswapfile --size "${SIZE}G" /swapfile
+sudo btrfs inspect-internal map-swapfile /swapfile
+echo '/swapfile none swap sw 0 0' | sudo tee -a /etc/fstab >/dev/null
 
 echo "Configuring swappiness and vfs_cache_pressure..."
-echo 'vm.swappiness=10' | sudo tee -a /etc/sysctl.d/00-custom.conf
-echo 'vm.vfs_cache_pressure=50' | sudo tee -a /etc/sysctl.d/00-custom.conf
+echo 'vm.swappiness=10' | sudo tee -a /etc/sysctl.d/00-custom.conf >/dev/null
+echo 'vm.vfs_cache_pressure=50' | sudo tee -a /etc/sysctl.d/00-custom.conf >/dev/null
 
 echo "Starting zramswap..."
 sudo swapon /swapfile
+sudo systemctl daemon-reload
+sudo systemctl start systemd-zram-setup@zram0 >/dev/null
 
 echo "Swap configured."
