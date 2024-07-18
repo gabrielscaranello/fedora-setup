895187ce78084bbc67e3f638bfd2f209ebc6e373
Gabriel Scaranello <gabrieljscaranello@gmail.com>
2024-07-17 23:11:15 -0300

    chore: add flathub repo script

--- scripts/install-flatpak-packages.sh
+++ scripts/install-flatpak-packages.sh
@@ -6,6 +6,7 @@ PACKAGES=$(cat "$PWD/flatpak-packages" | tr '\n' ' ')
 echo "Installing Flatpak packages..."
 
 echo "Installing..."
+sudo flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
 sudo flatpak install -y --system flathub $PACKAGES
 
 sudo flatpak override --system --filesystem=xdg-config/gtk-3.0:ro
