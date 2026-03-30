737af6d79aa0f150e1f937519262d659503c060b
Gabriel Scaranello <gabrieljscaranello@gmail.com>
2026-03-30 18:00:15 -0300

    fix: gnome extension install script

--- scripts/install-gnome-extensions.sh
+++ scripts/install-gnome-extensions.sh
@@ -26,35 +26,8 @@ _install_gnome_extensions() {
   done
 }
 
-_install_pano_extension() {
-  NVM_PATH="$HOME/.nvm/nvm.sh"
-  PANO_GIT_URL=https://github.com/oae/gnome-shell-pano.git
-  PANO_TMP_DIR=/tmp/pano
-  EXTENSION_DIR=$HOME/.local/share/gnome-shell/extensions
-  EXTENSION_TARGET="$EXTENSION_DIR/pano@elhan.io"
-
-  if [ ! -f "$NVM_PATH" ]; then
-    echo "NVM not found. Please install NVM and Node."
-    exit 0
-  fi
-
-  source "$NVM_PATH"
-
-  echo "Installing Pano extension..."
-  echo "Removing old files if exists..."
-  rm -rf "$PANO_TMP_DIR" "$EXTENSION_TARGET"
-
-  echo "Cloning Pano..."
-  git clone --depth=1 "$PANO_GIT_URL" "$PANO_TMP_DIR"
-
-  echo "Installing..."
-  mkdir -p "$EXTENSION_DIR"
-  cd "$PANO_TMP_DIR" && yarn install && yarn build
-  mv "$PANO_TMP_DIR/dist" "$EXTENSION_TARGET"
-}
-
 echo "Installing gnome extensions..."
 _install_gnome_shell_extension_intaller
 _install_gnome_extensions
-_install_pano_extension
 echo "GNOME extensions installed."
+
